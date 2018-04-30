ee028e0b9b8637b628479b0549f3600cc8b50701
lee <charismafight@hotmail.com>
2018-04-30 23:58:08 +0800

    add controller and spring mvc config

--- src/main/java/spring_in_practise/ch03/web/RosterController.java
+++ src/main/java/spring_in_practise/ch03/web/RosterController.java
@@ -1,7 +1,32 @@
 package spring_in_practise.ch03.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring_in_practise.ch03.model.Member;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class RosterController {
+    private List<Member> members = new ArrayList<>();
+
+    public RosterController() {
+        members.add(new Member("John", "Lennon"));
+        members.add(new Member("Paul", "McCartney"));
+        members.add(new Member("George", "Harrison"));
+        members.add(new Member("Ringo", "Starr"));
+    }
+
+    @RequestMapping
+    public void list(Model model) {
+        model.addAttribute(members);
+    }
+
+    @RequestMapping
+    public void member(@RequestParam int id, Model model) {
+        model.addAttribute(members.get(id));
+    }
 }
