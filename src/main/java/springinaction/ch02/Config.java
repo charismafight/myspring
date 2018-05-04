d71bb1b2c89ec5a6405bcedc1d8c057658131882
charismafight@hotmail.com <charismafight@hotmail.com>
2018-05-05 00:43:32 +0800

    bean's java config

---
+++ src/main/java/springinaction/ch02/Config.java
@@ -0,0 +1,10 @@
+package springinaction.ch02;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Config {
+
+}
