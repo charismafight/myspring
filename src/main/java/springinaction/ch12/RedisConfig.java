4d401e46981b9a44821b9e236adea4542f49f64a
charismafight@hotmail.com <charismafight@hotmail.com>
2018-06-15 23:22:11 +0800

    redis cache config

--- src/main/java/springinaction/ch12/RedisConfig.java
+++ src/main/java/springinaction/ch12/RedisConfig.java
@@ -1,7 +1,10 @@
 package springinaction.ch12;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -10,16 +13,32 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new JedisConnectionFactory(new RedisStandaloneConfiguration());
     }
 
+//    @Bean
+//    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate redis = new RedisTemplate();
+//        redis.setConnectionFactory(redisConnectionFactory);
+//        return redis;
+//    }
+
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate redis = new RedisTemplate();
-        redis.setConnectionFactory(redisConnectionFactory);
-        return redis;
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        JedisConnectionFactory factory=new JedisConnectionFactory();
+        factory.afterPropertiesSet();
+        return factory;
     }
 }
